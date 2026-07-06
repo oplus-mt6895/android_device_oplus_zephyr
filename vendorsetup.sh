c610b496366ee8bffafe4ef68ccf8cc8e8adb208
yuki <asaxxhiii@gmail.com>
2026-07-06 20:06:48 +0530

    Enhance vendorsetup.sh for 4K60FPS and ViPER4AndroidFX
    Added functionality to unlock 4K60FPS and clone ViPER4AndroidFX.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,5 +9,12 @@ cd vendor/lineage-priv/keys
 echo "no" | ./generate.sh
 cd ../../..
 
+echo "Unlock 4K60FPS"
+cd packages/apps/Aperture
+if ! git merge-base --is-ancestor 9509277efc852ad8bdcce204e0d9cfe104b6d190 HEAD 2>/dev/null; then
+    git fetch https://github.com/Adarsh0127-Elite/android_packages_apps_Aperture.git
+    git cherry-pick 9509277efc852ad8bdcce204e0d9cfe104b6d190
+fi
+
 echo "Cloning ViPER4AndroidFX"
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
