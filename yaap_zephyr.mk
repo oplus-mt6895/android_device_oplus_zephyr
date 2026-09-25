08224b16759be311e9c54c33e3c614e8347d6c09
almightygodthor <adityasingh6901012a@gmail.com>
2026-09-25 15:13:58 +0000

    zephyr: identify device as realme GT NEO 3

--- yaap_zephyr.mk
+++ yaap_zephyr.mk
@@ -7,14 +7,14 @@ $(call inherit-product, device/oplus/zephyr/device.mk)
 # Inherit from the YAAP configuration.
 $(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_zephyr
+PRODUCT_NAME := yaap_zephyr
 PRODUCT_DEVICE := zephyr
-PRODUCT_MANUFACTURER := OPlus
-PRODUCT_BRAND := OPlus
-PRODUCT_MODEL := mt6895
-PRODUCT_SYSTEM_NAME := zephyr
+PRODUCT_MANUFACTURER := realme
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3563
+PRODUCT_SYSTEM_NAME := RMX3563
 
-PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=OnePlus/PGKM10/OP5565:15/AP3A.240617.008/S.1f56c75-1-a469:user/release-keys \
